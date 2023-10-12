c982c089283929fa47845254fb8328ad87fb05df
tomaslmz <115982948+tomaslmz@users.noreply.github.com>
2023-10-11 21:52:10 -0300

    lembrete

--- src/models/ContaCorrente.java
+++ src/models/ContaCorrente.java
@@ -4,6 +4,26 @@ public class ContaCorrente extends ContaBancaria {
 	private float chequeEspecial;
 	private double taxaManutencao = 0.03;
 	
+	public void ContaCorrente(int id, int numeroConta, float saldo, String titular, int agencia, float chequeEspecial, double TaxaManutencao) {
+//		fazer isso
+	}
+	
+	public float getChequeEspecial() {
+		return chequeEspecial;
+	}
+
+	public void setChequeEspecial(float chequeEspecial) {
+		this.chequeEspecial = chequeEspecial;
+	}
+
+	public double getTaxaManutencao() {
+		return taxaManutencao;
+	}
+
+	public void setTaxaManutencao(double taxaManutencao) {
+		this.taxaManutencao = taxaManutencao;
+	}
+
 	private void aplicarTaxaManutencao() {
 		
 	}
