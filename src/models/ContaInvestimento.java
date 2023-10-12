7c4c8bfedf21be9a983c9f7c4acb168664902b10
tomaslmz <115982948+tomaslmz@users.noreply.github.com>
2023-10-11 21:33:54 -0300

    criacao do model ContaInvestimento

---
+++ src/models/ContaInvestimento.java
@@ -0,0 +1,13 @@
+package models;
+
+public class ContaInvestimento {
+	String[] investimentos;
+	
+	public void investir(float valor, int tipo) {
+		
+	}
+	
+	public String getInvestimento(int tipo) {
+		return "SEM CONEXAO";
+	}
+}
