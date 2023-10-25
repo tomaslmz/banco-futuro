6ac7ec87dd1c4ca20f9ae97b4d98f373c27a75a7
tomaslmz <115982948+tomaslmz@users.noreply.github.com>
2023-10-25 19:36:33 -0300

    fixing date bug of pessoa class

--- src/models/Pessoa.java
+++ src/models/Pessoa.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Pattern;
@@ -95,14 +96,19 @@ public class Pessoa {
 
     public void setDataNascimento(String dataNascimento) {
         try {
-            if(!Pattern.compile("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$").matcher(dataNascimento).find()) {
+            if (!Pattern.compile("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$")
+                    .matcher(dataNascimento).find()) {
                 throw new Exception("Data de nascimento inválido!");
             }
 
-            this.dataNascimento = LocalDateTime.parse(dataNascimento, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        } catch(Exception e) {
+            String dataFormatada = dataNascimento.replaceAll("[/\\.-]", "/");
+
+            this.dataNascimento = LocalDate.parse(dataFormatada, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                .atStartOfDay();
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
+
+    
 }
